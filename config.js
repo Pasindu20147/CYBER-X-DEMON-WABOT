ac0b606bcb5bb71c5a25263b6a5cc2d9a527c3b8
FRAMEXTEAM <darkframexteam@gmail.com>
2025-02-06 16:21:46 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "file:///D:/FRAMEXTEAM/Annotation%202024-12-30%20223823.png",
+ALIVE_IMG: process.env.ALIVE_MSG || "Hello, I am CYBER X DEMON WABOT I am alive now!",
 };
